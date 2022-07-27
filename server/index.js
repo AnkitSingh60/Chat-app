65b70d2a57c2d2d8ea08cba67b48788f5fc92557
AnkitSingh60 <ankitanwar730@gmail.com>
2022-07-27 14:42:37 +0530

    added dummy data and fetched in postman

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 app.get("/api/chat", (req, res) => {
     res.status(200).send(chats)
 })
-
+    
 app.get("/api/chat/:id", (req, res) => {
     const singleChat = chats.find((chat)=> chat._id === req.params.id)
     res.status(200).send(singleChat)
